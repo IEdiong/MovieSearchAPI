36ee166601dc561421790ebadc35b40ee6d8e644
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-02-15 15:49:10 +0100

    add api for search for movies

---
+++ MovieSearchAPI/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieSearchAPI.Data;
+
+
+namespace MovieSearchAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MoviesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        // GET: /<controller>/
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies(string s)
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var uri = $"http://www.omdbapi.com/?apikey=d05aeeac&s={s}";
+                //var uri = $"http://www.omdbapi.com/?apikey={process.env.NEXT_PUBLIC_API_KEY}&${searchQuery}";
+
+                var response = await httpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    // Process the responseData as needed
+                    return Ok(responseData);
+                }
+                else
+                {
+                    // Handle the error response
+                    return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
---
+++ MovieSearchAPI/Data/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace MovieSearchAPI.Data
+{
+	public class Movie
+	{
+		public string Title { get; set; } = String.Empty;
+        public string Year { get; set; } = String.Empty;
+        public string ImdbID { get; set; } = String.Empty;
+        public string Type { get; set; } = String.Empty;
+        public string Poster { get; set; } = String.Empty;
+        public string Plot { get; set; } = String.Empty;
+        public string Rated { get; set; } = String.Empty;
+        public string Released { get; set; } = String.Empty;
+        public string Genre { get; set; } = String.Empty;
+        public string Actors { get; set; } = String.Empty;
+        public string Runtime { get; set; } = String.Empty;
+        public string Country { get; set; } = String.Empty;
+        public string Director { get; set; } = String.Empty;
+        public string Language { get; set; } = String.Empty;
+        public IEnumerable<Rating> Ratings { get; set; }
+    }
+
+    public class Rating
+    {
+        public RatingSource Source { get; set; }
+        public string Value { get; set; } = String.Empty;
+    }
+
+    public enum RatingSource
+    {
+        InternetMovieDatabase,
+        RottenTomatoes,
+        Metacritic
+    }
+}
+
