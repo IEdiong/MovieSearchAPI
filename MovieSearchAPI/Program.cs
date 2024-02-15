a600135021405eb088fd2e993d8088a1ff7bd1f9
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-02-15 16:59:37 +0100

    hotfix: add new CORS fix

--- MovieSearchAPI/Program.cs
+++ MovieSearchAPI/Program.cs
@@ -4,13 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.ConfigureCors();
+//builder.Services.ConfigureCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
 
